9e69e1635a53a4e276b2da1ab953119329b8f43e
santiago cortes <jeissoncortes02@outlook.com>
2021-11-08 10:26:49 -0500

    add validation of the model

--- src/main/java/com/jeisson/server/model/Server.java
+++ src/main/java/com/jeisson/server/model/Server.java
@@ -1,9 +1,22 @@
 package com.jeisson.server.model;
 
 import com.jeisson.server.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Server {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "The ipAddress cannot be empy or null")
     private String ipAddress;
     private String name;
     private String memory;
