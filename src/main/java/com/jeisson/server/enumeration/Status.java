8612b307996fa8ae95732564a908f42237c1fd18
santiago cortes <jeissoncortes02@outlook.com>
2021-11-08 10:21:11 -0500

    defined the class Server and Enumeration Status

---
+++ src/main/java/com/jeisson/server/enumeration/Status.java
@@ -0,0 +1,16 @@
+package com.jeisson.server.enumeration;
+
+public enum Status {
+    SERVER_UP("SERVER_UP"),
+    SERVER_DOWN("SERVER_DOWN");
+
+    private final String status;
+
+    Status(String status){
+        this.status = status;
+    }
+
+    String getStatus(){
+        return this.getStatus();
+    }
+}
---
+++ src/main/java/com/jeisson/server/model/Server.java
@@ -0,0 +1,13 @@
+package com.jeisson.server.model;
+
+import com.jeisson.server.enumeration.Status;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
