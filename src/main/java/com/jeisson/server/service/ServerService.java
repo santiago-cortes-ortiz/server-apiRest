20bf0970722ffe09fcb9afd5ac5c73ea7bcded3f
santiago cortes <jeissoncortes02@outlook.com>
2021-11-08 13:30:29 -0500

    defined the services of servers

---
+++ src/main/java/com/jeisson/server/service/ServerService.java
@@ -0,0 +1,13 @@
+package com.jeisson.server.service;
+
+import com.jeisson.server.model.Server;
+
+import java.util.Collection;
+
+public interface ServerService {
+    Server create(Server server);
+    Collection<Server> list(int limit);
+    Server get(Long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+}
